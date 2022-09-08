ef317a0edf5a14944a972fefe0435b4a4c1ab4af
Baker Khoder <bakerkhoder878@gmail.com>
2022-09-08 16:59:29 +0300

    adding darken background with the popup

--- login.js
+++ login.js
@@ -1,12 +1,13 @@
 addEventListener("load",()=>{
  const popupbtn=document.getElementById("popupbtn")
  const popup=document.getElementById("popup")
- 
+ const darkenmask=document.getElementById("page-mask")
 
  
  popupbtn.addEventListener("click",()=>{
     console.log("hi")
     popup.classList.add("display")
+    darkenmask.classList.add("page-mask")
  }) 
     
  
